4dc34bd57be9016aafd67ad546ff248c097b54fd
Ramy Ardya <161923530+UltraRamy@users.noreply.github.com>
2025-03-28 09:57:06 +0700

    Implement delete function in Subscriber repository.

--- src/repository/subsrciber.rs
+++ src/repository/subsrciber.rs
@@ -10,7 +10,7 @@ lazy_static! {
 pub struct SubscriberRepository;
 
 impl SubscriberRepository {
-    pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
+     pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
             let subscriber_value = subscriber.clone();
             if SUBSCRIBERS.get(product_type).is_none() {
                 SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
@@ -20,7 +20,7 @@ impl SubscriberRepository {
                 .insert(subscriber_value.url.clone(), subscriber_value);
             return subscriber;
         }
-    pub fn list_all(product_type: &str) -> Vec<Subscriber> {
+     pub fn list_all(product_type: &str) -> Vec<Subscriber> {
              if SUBSCRIBERS.get(product_type).is_none() {
                  SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
              };
@@ -28,4 +28,15 @@ impl SubscriberRepository {
              return SUBSCRIBERS.get(product_type).unwrap().iter()
                  .map(|f| f.value().clone()).collect();
          }
+     pub fn delete(product_type: &str, url: &str) -> Option<Subscriber> {
+              if SUBSCRIBERS.get(product_type).is_none() {
+                  SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+              }
+              let result = SUBSCRIBERS.get(product_type).unwrap()
+                  .remove(url);
+              if !result.is_none() {
+                  return Some(result.unwrap().1);
+              }
+              return None;
+          }
     }
